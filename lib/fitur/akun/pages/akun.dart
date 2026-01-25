b3bae9dcbcbe115c9b8af10cc87070a76a873a17
Indra <draa24874@gmail.com>
2026-01-25 13:14:39 +0700

    perbaikan_logika

--- lib/fitur/akun/pages/akun.dart
+++ lib/fitur/akun/pages/akun.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // ✅ TAMBAHAN
+import 'package:cloud_firestore/cloud_firestore.dart'; 
 import '../../login/pages/login.dart';
 
 class ProfilPage extends StatefulWidget {
